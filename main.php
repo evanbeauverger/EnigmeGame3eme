62716b97ccc859bb7a9cba951a1027709fe77215
beauverger <beauverger@sio.ndlp>
2025-09-10 14:32:23 +0200

    first commit

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le jeu des IA</title>
+</head>
+<style>
+</style>
+<body>
+    <h1>Le jeu des IA</h1>
+    <a href="enseignant.php">Enseignant</a>
+    <p>Bienvenue sur le jeu des IA</p>
+    <a href="jeu.php">Commencer le jeu</a>
+</body>
+</html>
