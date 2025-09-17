f33df7c478a9b734cf681a7f227fb0aeca2ff7d3
beauverger <beauverger@sio.ndlp>
2025-09-17 14:59:01 +0200

    debut du projet

---
+++ src/Controller/JeuEnigmeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class JeuEnigmeController extends AbstractController
+{
+    #[Route('/', name: 'app_jeu_enigme')]
+    public function index(): Response
+    {
+        return $this->render('jeu_enigme/index.html.twig', [
+            'controller_name' => 'JeuEnigmeController',
+        ]);
+    }
+
+    #[Route('/jeu', name: 'app_jeu')]
+    public function jeu(): Response
+    {
+        return $this->render('jeu_enigme/jeu.html.twig');
+    }
+
+    #[Route('/jeu/enigme/{$id}', name: 'app_enigme')]
+    public function enigme(int $id): Response
+    {
+        return $this->render('jeu_enigme/jeu.html.twig');
+    }
+
+    #[Route('/enseignant', name: 'app_enseignant')]
+    public function enseignant(): Response
+    {
+        return $this->render('jeu_enigme/enseignant.html.twig');
+    }
+}
