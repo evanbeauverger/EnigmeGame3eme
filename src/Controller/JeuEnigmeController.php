cda383f34e76c4ce16e5ccbbfd2371cc24ec7f6f
beauverger <beauverger@sio.ndlp>
2025-11-05 14:48:35 +0100

    ajout de creation d'equipe

--- src/Controller/JeuEnigmeController.php
+++ src/Controller/JeuEnigmeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Team;
+use App\Form\TeamType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class JeuEnigmeController extends AbstractController
 {
+
+    public function __construct(
+        private FormFactoryInterface $formFactory
+    ){}
+
     #[Route('/', name: 'app_jeu_enigme')]
     public function index(): Response
     {
@@ -18,6 +28,23 @@ final class JeuEnigmeController extends AbstractController
     public function equipe(): Response
     {
         return $this->render('jeu_enigme/equipe.html.twig');
+    }
+
+    #[Route('/team_create', name: 'app_team_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $team = new Team();
+        $form = $this->formFactory->create(TeamType::class, $team);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($team);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_jeu');
+        }
+
+        return $this->render('jeu_enigme/create.html.twig', ['form' => $form->createView()]);
     }
 
     #[Route('/jeu', name: 'app_jeu')]
