f5612faa607b9152ff81a622561afac997cb7940
beauverger <beauverger@sio.ndlp>
2025-11-06 16:53:10 +0100

    commencement des rôles et de la sécurité

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -65,7 +65,11 @@ class User
 
     public function getRole(): array
     {
-        return $this->role;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRole(array $role): static
